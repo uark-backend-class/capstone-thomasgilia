9c3510b20c1c0107e0bedd5fca21cdd2755431fe
Gilia <Thomas.gilia@gmail.com>
2020-03-21 21:15:08 -0500

    fixed issue with note mispopulating in listall view.

--- messAroundFile.js
+++ messAroundFile.js
@@ -1,16 +1,2 @@
-
-      {{#each resources }}
-      <tr>
-        <td>{{this.title}}</td>
-        <td>{{this.id}}</td>
-        <td>
-          {{#if isNote}}
-          <a class="btn btn-secondary btn-sm" href="/notes/note{{ this.id }}">View Note</a>
-          {{else}}
-          <a class="btn btn-secondary btn-sm" href="/docs/doc{{ this.id }}">View Document</a>
-          {{/if}}
-        </td>
-      </tr>
-      {{/each}}
-    </table>
-  </div>
+<!--{{else}}
+          <a class="btn btn-secondary btn-sm" href="/docs/doc{{ this.id }}">View Document</a> -->
